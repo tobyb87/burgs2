16aaede2f7cf41db080e8324320bf2cfa9d9476a
Toby Boyd <tobyboyd87@gmail.com>
2017-06-11 05:21:22 -0500

    adding it all

---
+++ models/burger.js
@@ -0,0 +1,18 @@
+
+
+module.exports = function(sequelize, DataTypes) {
+  var Burger = sequelize.define("Burger", {
+    burger_name: DataTypes.STRING,
+    devoured: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return Burger;
+};
